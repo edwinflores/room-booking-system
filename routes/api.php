5351ba8307b7e8431936ace3e3c193dd710e10e9
edwinflores <edwinjohn.flores@gmail.com>
2024-05-14 20:11:39 +0900

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\BookingsController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Auth::shouldUse('auth0-api');
+
+Route::group(['prefix' => 'bookings'], function ($router) {
+    Route::get('rooms', [BookingsController::class, 'showPublicMessage']);
+
+    // Route::group(['middleware' => 'auth'], function () {
+    //     Route::get('protected', [MessagesController::class, 'showProtectedMessage']);
+    //     Route::get('admin', [MessagesController::class, 'showAdminMessage'])->can('read:admin-messages');
+    // });
+});
